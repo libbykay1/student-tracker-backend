3f8fc986d4cdc889b8aa00365d333aecdf20b7a5
Libby Eilbert <elizabeth.eilbert@gmail.com>
2025-03-29 09:48:53 -1000

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
